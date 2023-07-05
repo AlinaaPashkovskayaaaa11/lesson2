34995cb46ca851a2e089e867305424acfe0f89aa
Alina <alinbl4@mail.ru>
2023-07-05 22:50:54 +0300

    Homework2

---
+++ src/main/java/Lesson2/Age2.java
@@ -0,0 +1,37 @@
+package Lesson2;
+
+import java.util.Scanner;
+
+public class Age2 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int i = in.nextInt();
+        if (i==1) {
+                System.out.println("Январь");}
+        else if (i==2) {
+            System.out.println("Февраль");
+        } else if (i==3) {
+            System.out.println("Март");
+        } else if (i==4) {
+            System.out.println("Апрель");
+        } else if (i==5) {
+            System.out.println("Май");
+        } else if (i==6) {
+            System.out.println("Июнь");
+        } else if (i==7) {
+            System.out.println("Июль");
+        } else if (i==8) {
+            System.out.println("Август");
+        } else if (i==9) {
+            System.out.println("Сентябрь");
+        } else if (i==10) {
+            System.out.println("Октябрь");
+        } else if (i==11) {
+            System.out.println("Ноябрь");
+        }else if (i==12) {
+            System.out.println("Декабрь");
+        }
+
+    }
+}
