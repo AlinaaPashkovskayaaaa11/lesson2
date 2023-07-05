7b4ce22111f7cea492d31339b20156fafb46fe52
Alina <alinbl4@mail.ru>
2023-07-05 22:56:15 +0300

    Homework2

---
+++ src/main/java/Lesson2/Number10.java
@@ -0,0 +1,23 @@
+package Lesson2;
+
+
+
+        
+
+public class Number10 {
+
+    private static int[][] MULTIPLICATION_TABLE = new int [10][10];
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                MULTIPLICATION_TABLE[i][j]=(i+1)*(j+1);
+                System.out.print(MULTIPLICATION_TABLE[i][j]+" ");
+
+            }System.out.println();
+
+        }
+    }
+}
+
+
