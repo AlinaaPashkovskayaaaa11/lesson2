fc058119672bc1a9b166c7aaa941afef6c6e975e
Alina <alinbl4@mail.ru>
2023-07-05 22:55:48 +0300

    Homework2

---
+++ src/main/java/Lesson2/Number8.java
@@ -0,0 +1,17 @@
+package Lesson2;
+
+import java.util.Scanner;
+
+public class Number8 {
+    public static void main(String[] args) {
+     int number = 7;
+     int power = 0;
+     int result = 0;
+      while (power<14){
+          result=result+number;
+          System.out.println(result);
+          power++;
+      }
+
+      }
+}
